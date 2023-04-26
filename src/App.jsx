d2b8d76710af6f67d0fbd2f5fb31eb9f83f64e58
Luís Felipe de Ávila e Silva <83726056+luisavila97@users.noreply.github.com>
2023-04-26 14:31:41 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,19 @@ function App() {
   const [queryArray, setQueryArray] = useState([]);
 
   const url = `https://api.github.com/users/${userList.slice(-1)}`;
-  const { data } = useQuery([`user-${userList.slice(-1)}`, userList], async () => {
+
+  const fetchData = async () => {
     const data = await fetch(url);
     return data.json();
-  });
+  };
+
+  const { data } = useQuery(
+    [`user-${userList.slice(-1)}`, userList],
+    fetchData,
+    {
+      enabled: !!userList,
+    }
+  );
 
   useEffect(() => {
     if (data) {
