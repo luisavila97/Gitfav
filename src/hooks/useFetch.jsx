cc78599f2b6ac75d13517b67579a1d45f0b21b3b
Luís Felipe de Ávila e Silva <83726056+luisavila97@users.noreply.github.com>
2023-04-27 18:59:46 -0300

    Created custom hook to Fetch data

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
+
+const useFetch = (user, userList, setQueryArray, queryArray) => {
+  const url = `https://api.github.com/users/${user}`;
+
+  const fetchData = async () => {
+    const data = await fetch(url);
+    return data.json();
+  };
+
+  const { data } = useQuery([`user-${user}`, userList], fetchData, {
+    enabled: !!user,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setQueryArray([...queryArray, data]);
+    }
+  }, [data]);
+};
+
+export default useFetch;
